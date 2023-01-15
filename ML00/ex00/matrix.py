383c9cc9e215e13845f6f66f221d209c16edc8a0
Benoit Savinel <bsavinel@student.42.fr>
2023-01-15 16:10:05 +0100

    ML00/ex00 fini

--- ML00/ex00/matrix.py
+++ ML00/ex00/matrix.py
@@ -1,7 +1,9 @@
 class MatrixException(Exception):
 	def __init__(self, message):
 		self.message = message
-		super.__init__(self.message)
+		super().__init__(self.message)
+
+
 
 #!####################################################################################################!#
 #!#############################################  MATRIX  #############################################!#
@@ -27,17 +29,21 @@ class Matrix:
 
 	def __add__(self, other):
 		if (isinstance(other, Matrix)):
+			if ((not isinstance(self, Vector)) and isinstance(other, Vector)):
+				return NotImplemented
 			if (self.shape != other.shape):
 				raise MatrixException("Matrix can be added only if they have the same shape")
-			return type([[self.data[j][i] + other.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
+			return type(self)([[self.data[j][i] + other.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
 		else:
 			raise MatrixException("Matrix can be added only with another matrix")
 
 	def __radd__(self, other):
 		if (isinstance(other, Matrix)):
+			if ((not isinstance(self, Vector)) and isinstance(other, Vector)):
+				return NotImplemented
 			if (self.shape != other.shape):
 				raise MatrixException("Matrix can be added only if they have the same shape")
-			return Matrix([[other.data[j][i] + self.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
+			return type(self)([[other.data[j][i] + self.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
 		else:
 			raise MatrixException("Matrix can be added only with another matrix")
 
@@ -45,17 +51,21 @@ class Matrix:
 
 	def __sub__(self, other):
 		if (isinstance(other, Matrix)):
+			if ((not isinstance(self, Vector)) and isinstance(other, Vector)):
+				return NotImplemented
 			if (self.shape != other.shape):
 				raise MatrixException("Matrix can be sub only if they have the same shape")
-			return type([[self.data[j][i] - other.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
+			return type(self)([[self.data[j][i] - other.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
 		else:
 			raise MatrixException("Matrix can be sub only with another matrix")
 
 	def __rsub__(self, other):
 		if (isinstance(other, Matrix)):
+			if ((not isinstance(self, Vector)) and isinstance(other, Vector)):
+				return NotImplemented
 			if (self.shape != other.shape):
 				raise MatrixException("Matrix can be sub only if they have the same shape")
-			return type([[other.data[j][i] - self.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
+			return type(self)([[other.data[j][i] - self.data[j][i] for i in range(self.shape[1])] for j in range(self.shape[0])])
 		else:
 			raise MatrixException("Matrix can be sub only with another matrix")
 
@@ -63,7 +73,7 @@ class Matrix:
 
 	def __truediv__(self, scalar):
 		if (isinstance(scalar, int) or isinstance(scalar, float)):
-			return type([[self.data[j][i] / scalar for i in range(self.shape[1])] for j in range(self.shape[0])])
+			return type(self)([[self.data[j][i] / scalar for i in range(self.shape[1])] for j in range(self.shape[0])])
 		else:
 			raise MatrixException("Matrix can be divided only by a scalar")
 
@@ -78,8 +88,10 @@ class Matrix:
 				return NotImplemented
 			if (self.shape[1] != other.shape[0]):
 				raise MatrixException("Matrix can be multiplied only if the first matrix have the same number of columns as the second matrix have of rows")
-			return type([sum(self.data[j][i] * other.data[i][k] for i in range(self.shape[1])) for j in range(self.shape[0])] for k in range(other.shape[1]))
-		raise MatrixException("Matrix can be multiplied only with another matrix")
+			return type(self)([[sum(self.data[j][i] * other.data[i][k] for i in range(self.shape[1])) for k in range(other.shape[1])] for j in range(self.shape[0])])
+		elif (isinstance(other, int) or isinstance(other, float)):
+			return type(self)([[self.data[j][i] * other for i in range(self.shape[1])] for j in range(self.shape[0])])
+		raise MatrixException("Matrix can be multiplied only with another matrix or a scalar")
 	
 	def __rmul__(self, other):
 		if (isinstance(other, Matrix)):
@@ -87,29 +99,31 @@ class Matrix:
 				return NotImplemented
 			if (self.shape[0] != other.shape[1]):
 				raise MatrixException("Matrix can be multiplied only if the first matrix have the same number of columns as the second matrix have of rows")
-			return type([sum(other.data[j][i] * self.data[i][k] for i in range(self.shape[0])) for j in range(other.shape[0])] for k in range(self.shape[1]))
-		raise MatrixException("Matrix can be multiplied only with another matrix")
+			return type(self)([[sum(other.data[j][i] * self.data[i][k] for i in range(self.shape[0])) for k in range(self.shape[1])] for j in range(other.shape[0])])
+		elif (isinstance(other, int) or isinstance(other, float)):
+			return type(self)([[self.data[j][i] * other for i in range(self.shape[1])] for j in range(self.shape[0])])
+		raise MatrixException("Matrix can be multiplied only with another matrix or a scalar")
 
 #!----------------Matrix string----------------!#
 
 	def __str__(self):
-		str = "Matrix size: " + str(self.shape) + "\n Content : "
+		string = "Matrix size : " + str(self.shape) + "\nContent : "
 		for i in range(self.shape[0]):
-			str += "\n"
-			str += str(self.data[i])
-		return str
+			string  += "\n"
+			string  += str(self.data[i])
+		return string 
 
 	def __repr__(self):
-		str = "Matrix size: " + str(self.shape) + "\n Content : "
+		string  = "Matrix size : " + str(self.shape) + "\nContent : "
 		for i in range(self.shape[0]):
-			str += "\n"
-			str += str(self.data[i])
+			string  += "\n"
+			string  += str(self.data[i])
 		return str
 
 #!----------------Matrix transposition----------------!#
 
 	def	T(self):
-		return type([self.data[i][j] for i in range(self.shape[0])] for j in range(self.shape[1]))
+		return type(self)([[self.data[i][j] for i in range(self.shape[0])] for j in range(self.shape[1])])
 
 #!####################################################################################################!#
 #!#############################################  VECTOR  #############################################!#
@@ -128,6 +142,8 @@ class Vector(Matrix):
 			if (self.shape[0] != 1 and self.shape[1] != 1):
 				raise MatrixException("Vector need to have only one row or one column")
 		elif (isinstance(matrix_data, tuple) and len(matrix_data) == 2 and matrix_data[0] > 0 and matrix_data[1] > 0):
+			if (matrix_data[0] != 1 and matrix_data[1] != 1):
+				raise MatrixException("Vector need to have only one row or one column")
 			self.shape = matrix_data
 			self.data = [[0 for i in range(self.shape[1])] for j in range(self.shape[0])]
 		else:
@@ -136,14 +152,14 @@ class Vector(Matrix):
 #!----------------Vector string----------------!#
 
 	def __str__(self):
-		str = "Vector size: " + str(self.shape) + "\n Content : "
+		string  = "Vector size : " + str(self.shape) + "\nContent : "
 		for i in range(self.shape[0]):
-			str += "\n"
-			str += str(self.data[i])
-		return str
+			string  += "\n"
+			string  += str(self.data[i])
+		return string 
 
 	def __repr__(self):
-		str = "Vector size: " + str(self.shape) + "\n Content : "
+		str = "Vector size : " + str(self.shape) + "\nContent : "
 		for i in range(self.shape[0]):
 			str += "\n"
 			str += str(self.data[i])
--- ML00/ex00/test.py
+++ ML00/ex00/test.py
@@ -1,6 +1,161 @@
 from matrix import *
 
-if __name__ == "__main__":
-	matrix = Matrix((2, 3))
-	print(matrix.data)
-	print(matrix.shape)
+def printListInMatix(string, listM):
+	if (isinstance(string, str) and isinstance(listM, list)):
+		print(string)
+		print("size : (", len(listM), "," , len(listM[0]), ")\nContent :")
+		for i in range(len(listM)):
+			print(listM[i])
+		print("")
+	else:
+		return NotImplemented
+	
+print("""############################################
+############   Initialisation   ############
+############################################\n""")
+
+m1 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+print("m1 :")
+print(m1)
+m2 = Matrix((3, 2))
+print("\nm2 :")
+print(m2)
+try:
+	m3 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0]])
+except MatrixException as e:
+	print("\nError :", e)
+m4 = Vector((1, 3))
+print("\nm4 :")
+print(m4)
+m5 = Vector([[0.0], [1.0], [2.0]])
+print("\nm5 :")
+print(m5)
+m6 = Vector((3 , 1))
+print("\nm6 :")
+print(m6)
+try:
+	m7 = Vector((3,2))
+except MatrixException as e:
+	print("\nError :", e)
+
+print("\n-------   Result expected   -------\n")
+printListInMatix("m1 :", [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+printListInMatix("m2 :", [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
+print("Error because m3 is not rectangular\n")
+printListInMatix("m4 :", [[0.0, 0.0, 0.0]])
+printListInMatix("m5 :", [[0.0], [1.0], [2.0]])
+printListInMatix("m6 :", [[0.0], [0.0], [0.0]])
+print("Error because m7 is not a vector\n")
+
+print("""#####################################
+############   Adition   ############
+#####################################\n""")
+
+m1 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+m2 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+m3 = m1 + m2
+print("m3 :")
+print(m3)
+try:
+	m4 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0, 7.0]])
+	m5 = m1 + m4
+except MatrixException as e:
+	print("\nError :", e)
+
+print("\n-------   Result expected   -------\n")
+
+printListInMatix("m3 :", [[0.0, 2.0], [4.0, 6.0], [8.0, 10.0]])
+print("Error because m1 and m4 don't have the same dimmension\n")
+
+print("""##########################################
+############   substraction   ############
+##########################################\n""")
+
+m1 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+m2 = Matrix([[0.0, 2.0], [4.0, 6.0], [8.0, 10.0]])
+m3 = m2 - m1
+print("m3 :")
+print(m3)
+try:
+	m4 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0, 7.0]])
+	m5 = m1 - m4
+except MatrixException as e:
+	print("\nError :", e)
+
+print("\n-------   Result expected   -------\n")
+
+printListInMatix("m3 :", [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+print("Error because m1 and m4 don't have the same dimmension\n")
+
+print("""############################################
+############   Multiplication   ############
+############################################\n""")
+
+m1 = Matrix(([[0.0, 1.0, 2.0, 3.0], [0.0, 2.0, 4.0, 6.0]]))
+m2 = m1 * 2
+print("m2 :")
+print(m2)
+m3 = 2 * m1
+print("\nm3 :")
+print(m3)
+m4 = Matrix([[0.0, 1.0],[2.0, 3.0],[4.0, 5.0],[6.0, 7.0]])
+m5 = m1 * m4
+print("\nm5 :")
+print(m5)
+
+try:
+	m6 = Matrix([[0.0, 1.0, 2.0]])
+	m7 = m1 * m6
+except MatrixException as e:
+	print("\nError :", e)
+
+m7 = Matrix([[0.0, 1.0, 2.0], [0.0, 2.0, 4.0]])
+m8 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+v1 = Vector([[1], [2], [3]])
+v2 = Vector([[1, 2, 3]])
+v3 = m7 * v1
+v4 = v2 * m8
+print("\nv3 :")
+print(v3)
+print("\nv4 :")
+print(v4)
+
+print("\n-------   Result expected   -------\n")
+
+printListInMatix("m2 :", [[0.0, 2.0, 4.0, 6.0], [0.0, 4.0, 8.0, 12.0]])
+printListInMatix("m3 :", [[0.0, 2.0, 4.0, 6.0], [0.0, 4.0, 8.0, 12.0]])
+printListInMatix("m5 :", [[28., 34.], [56., 68.]])
+print("Error because m1 and m6 don't have the compatible dimension\n")
+printListInMatix("v3 :", [[8.], [16.]])
+printListInMatix("v4 :", [[16., 22.]])
+
+print("""######################################
+############   Division   ############
+######################################\n""")
+
+m1 = Matrix([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+m2 = m1 / 2
+print("m2 :")
+print(m2)
+
+print("\n-------   Result expected   -------\n")
+
+printListInMatix("m2 :", [[0.5, 1.0], [1.5, 2.0], [2.5, 3.0]])
+
+print("""###########################################
+############   Transposition   ############
+###########################################\n""")
+
+m1 = Matrix([[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+print("m1 :")
+print(m1)
+m2 = m1.T()
+print("\nm2 :")
+print(m2)
+m3 = m2.T()
+print("\nm3 :")
+print(m3)
+print("\n-------   Result expected   -------\n")
+printListInMatix("m1 :", [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
+printListInMatix("m2 :", [[0.0, 2.0, 4.0], [1.0, 3.0, 5.0]])
+printListInMatix("m3 :", [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0]])
