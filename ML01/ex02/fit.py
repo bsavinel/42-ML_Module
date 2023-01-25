d302e4080667d397e21558a625525ca7f0042544
Benoit Savinel <bsavinel@student.42.fr>
2023-01-25 15:09:33 +0100

    plus de description predict ML01/ex02

--- ML01/ex02/fit.py
+++ ML01/ex02/fit.py
@@ -10,18 +10,6 @@ def isVector(x):
 	return False
 
 def predict(x, theta):
-	"""Computes the vector of prediction y_hat from two non-empty numpy.array.
-	Args:
-		x: has to be an numpy.array, a vector of dimension m * 1.
-		theta: has to be an numpy.array, a vector of dimension 2 * 1.
-	Returns:
-		y_hat as a numpy.array, a vector of dimension m * 1.
-		None if x and/or theta are not numpy.array.
-		None if x or theta are empty numpy.array.
-		None if x or theta dimensions are not appropriate.
-	Raises:
-		This function should not raise any Exceptions.
-	"""
 	if ((not isinstance(x, np.ndarray)) or (not isinstance(theta, np.ndarray)) or x.size == 0 or theta.size != 2 or x.shape[1] != 1 or theta.ndim != 2):
 		return None
 	copyX = x.reshape(-1)
