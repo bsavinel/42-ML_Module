fde714e1d80ff1b8dca1191f96c57a71c1b4e9b8
Benoit Savinel <bsavinel@student.42.fr>
2023-01-26 17:40:36 +0100

    ML01/ex06 fini

---
+++ ML01/ex06/minmax.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def minmax(x):
+	"""Computes the normalized version of a non-empty numpy.ndarray using the min-max standardization.
+	Args:
+		x: has to be an numpy.ndarray, a vector.
+	Returns:
+		x' as a numpy.ndarray.
+		None if x is a non-empty numpy.ndarray or not a numpy.ndarray.
+	Raises:
+		This function shouldn't raise any Exception.
+	"""
+	if (not isinstance(x, np.ndarray) or x.size == 0):
+		return None
+	Xcopy = x.reshape(-1)
+	min = np.min(Xcopy)
+	max = np.max(Xcopy)
+	return (Xcopy - min) / (max - min)
+
+
+#!####################################################################################################!#
+#!##############################################  TEST  ##############################################!#
+#!####################################################################################################!#
+
+
+if __name__ == "__main__":
+	X = np.array([0, 15, -9, 7, 12, 3, -21]).reshape((-1, 1))
+	Y = np.array([2, 14, -13, 5, 12, 4, -19]).reshape((-1, 1))
+	ret1 = minmax(X)
+	ret2 = minmax(Y)
+	print(ret1)
+	print(ret2)
+	print("\n-------   Result expected   -------\n")
+	print(np.array([0.58333333, 1. , 0.33333333, 0.77777778, 0.91666667,0.66666667, 0. ]))
+	print(np.array([0.63636364, 1. , 0.18181818, 0.72727273, 0.93939394,0.6969697 , 0. ]))
