63f53dccdf65f328bbca44bafc996f9d03880834
Benoit Savinel <bsavinel@student.42.fr>
2023-02-03 11:23:52 +0100

    ML03/ex04 fini

---
+++ ML03/ex04/log_gradient.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+#!########################################################################################################!#
+#!##############################################  FUNCTION  ##############################################!#
+#!########################################################################################################!#
+
+def check_matrix(m, sizeX, sizeY, dim = 2):
+	if (not isinstance(m, np.ndarray)):
+		return False
+	if (m.ndim != dim or m.size == 0):
+		return False
+	if (sizeX != -1 and m.shape[0] != sizeX):
+		return False
+	if (dim == 2 and sizeY != -1 and m.shape[1] != sizeY):
+		return False
+	return True
+
+def sigmoid_(x):
+	"""
+	Compute the sigmoid of a vector.
+	Args:
+		x: has to be a numpy.ndarray of shape (m, 1).
+	Returns:
+		The sigmoid value as a numpy.ndarray of shape (m, 1).
+		None if x is an empty numpy.ndarray.
+	Raises:
+		This function should not raise any Exception.
+	"""
+	if (not check_matrix(x, -1, 1)):
+		return None
+	return 1 / (1 + np.exp(x * -1))	
+
+
+def log_gradient(x, y, theta):
+	"""Computes a gradient vector from three non-empty numpy.ndarray, with a for-loop. The three arrays must have compatibl
+	Args:
+		x: has to be an numpy.ndarray, a matrix of shape m * n.
+		y: has to be an numpy.ndarray, a vector of shape m * 1.
+		theta: has to be an numpy.ndarray, a vector of shape (n + 1) * 1.
+	Returns:
+		The gradient as a numpy.ndarray, a vector of shape n * 1, containing the result of the formula for all j.
+		None if x, y, or theta are empty numpy.ndarray.
+		None if x, y and theta do not have compatible dimensions.
+	Raises:
+		This function should not raise any Exception.
+	"""
+	if (not check_matrix(x, -1, -1) or not check_matrix(y, x.shape[0], 1) or not check_matrix(theta, x.shape[1] + 1, 1)):
+		return None
+	tmp = []
+	copyX = np.insert(x, 0, 1, axis = 1)
+	sigmoDot = sigmoid_(np.dot(copyX, theta)) - y
+	for i in range(theta.shape[0]):
+		tmp.append(np.sum(sigmoDot * copyX[:, i].reshape((-1, 1))) / x.shape[0])
+	return np.array(tmp).reshape((-1, 1))
+	
+#!####################################################################################################!#
+#!##############################################  TEST  ##############################################!#
+#!####################################################################################################!#
+
+if __name__ == '__main__':
+	y1 = np.array([1]).reshape((-1, 1))
+	x1 = np.array([4]).reshape((-1, 1))
+	y2 = np.array([[1], [0], [1], [0], [1]])
+	x2 = np.array([[4], [7.16], [3.2], [9.37], [0.56]])
+	y3 = np.array([[0], [1], [1]])
+	x3 = np.array([[0, 2, 3, 4], [2, 4, 5, 5], [1, 3, 2, 7]])
+	theta1 = np.array([[2], [0.5]])
+	theta2 = np.array([[2], [0.5]])
+	theta3 = np.array([[-2.4], [-1.5], [0.3], [-1.4], [0.7]])
+	ret1 = log_gradient(x1, y1, theta1)
+	print(ret1)
+	ret2 = log_gradient(x2, y2, theta2)
+	print(ret2)
+	ret3 = log_gradient(x3, y3, theta3)
+	print(ret3)
+	print("\n-------   Result expected   -------\n")
+	print(np.array([[-0.01798621],[-0.07194484]]))
+	print(np.array([[0.3715235 ],[3.25647547]]))
+	print(np.array([[-0.55711039],[-0.90334809],[-2.01756886],[-2.10071291],[-3.27257351]]))
