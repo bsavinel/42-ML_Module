8b5ab03de1f66f89966bdcb0b3165998499781d0
Benoit Savinel <bsavinel@student.42.fr>
2023-06-15 18:40:06 +0200

    ML03: exo7 partie 1 ok

---
+++ ML03/ex07/mono_log.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+from my_logistic_regression import check_matrix, MyLogisticRegression as MyLR
+import sys
+import random
+
+def unison_shuffled_copies(a, b, seed):
+	if (len(a) != len(b) or len(a) == 0):
+		return None
+	np.random.seed(seed)
+	p = np.random.permutation(len(a))
+	return a[p], b[p]
+
+def data_spliter(x, y, proportion, seed):
+	if (not check_matrix(x, -1, -1) or not check_matrix(y, x.shape[0], 1) or proportion > 1 or proportion < 0 or not isinstance(seed, int)):
+		return None
+	index_prop = int(x.shape[0] * proportion)
+	copyX, copyY = unison_shuffled_copies(x, y, seed)
+	return (copyX[:index_prop], copyX[index_prop:], copyY[:index_prop], copyY[index_prop:])
+
+def normalizer(x, list1):
+	if (not check_matrix(x, -1, 1) or not check_matrix(list1, -1, 1)):
+		print("error",x.shape)
+
+		return None
+	Xcopy = x.reshape(-1)
+	min = np.min(list1)
+	max = np.max(list1)
+	if (max == min):
+		raise ValueError("Normalizer: max and min of the array are equal")
+	return (Xcopy - min) / (max - min)
+
+def denormalizer(x, list1):
+	if (not check_matrix(x, -1, 1) or not check_matrix(list1, -1, 1)):
+		return None
+	Xcopy = x.reshape(-1)
+	min = np.min(list1)
+	max = np.max(list1)
+	return Xcopy * (max - min) + min
+
+def normalizer_multiline(x, list):
+	if (not check_matrix(x, -1, -1) or not check_matrix(list, -1, x.shape[1])):
+		return None
+	Xcopy = x.copy()
+	for i in range(x.shape[1]):
+		Xcopy[:, i] = normalizer(Xcopy[:, i].reshape(-1,1), list[:, i].reshape(-1,1))
+	return Xcopy
+
+#!#################################################################################################
+#!#####################################   Programe   ##############################################
+#!#################################################################################################
+
+zipcode = random.randint(0, 3)
+for (i, arg) in enumerate(sys.argv):
+	if (arg.startswith('-zipcode=') or arg.startswith('zipcode=–') and len(arg) == 10):
+		try:
+			zipcode = int(arg[9])
+		except:
+			print("Error: zipcode must be 0, 1, 2 or 3")
+		break
+
+if (zipcode == 0):
+	planet = "The flying cities of Venus"
+elif (zipcode == 1):
+	planet = "United Nations of Earth"
+elif (zipcode == 2):
+	planet = "Mars Republic"
+elif (zipcode == 3):
+	planet = "The Asteroids' Belt colonies"
+
+print("The zipcode", zipcode, "is for the citizen of", planet)
+
+progSeed = int(datetime.now().timestamp())
+data = pd.read_csv("solar_system_census.csv")
+result = pd.read_csv("solar_system_census_planets.csv")
+data = np.array(data[["weight","height","bone_density"]])
+result = np.array(result["Origin"])
+result = np.where(result == zipcode, 1, 0).reshape(-1, 1)
+Xtrain, Xeval, Ytrain, Yeval = data_spliter(data, result, 0.7, progSeed)
+XtrainNorm = normalizer_multiline(Xtrain, data)
+XevalNorm = normalizer_multiline(Xeval, data)
+
+
+myLR = MyLR(np.ones((4,1)), 0.1, 150000)
+myLR.fit_(XtrainNorm, Ytrain)
+Yhat = myLR.predict_(XevalNorm)
+YhatComp = np.where(Yhat >= 0.5, 1, 0)
+
+
+count = 0
+for i in range(Yeval.shape[0]):
+	if (Yeval[i] == YhatComp[i]):
+		count += 1
+print("Accuracy:", count / Yeval.shape[0])
+
+
+ax = plt.axes(projection='3d')
+Yeval = Yeval.reshape(-1)
+Yhat = Yhat.reshape(-1)
+ax.plot(Yeval, Xeval[:,0], Xeval[:,1], label="True value", marker='.', linestyle='None')
+ax.plot(Yhat, Xeval[:,0], Xeval[:,1], label="Prediction", marker='.', linestyle='None')
+ax.legend()
+plt.show()
+
+ax = plt.axes(projection='3d')
+ax.plot(Yeval, Xeval[:,1], Xeval[:,2], label="True value", marker='.', linestyle='None')
+ax.plot(Yhat, Xeval[:,1], Xeval[:,2], label="Prediction", marker='.', linestyle='None')
+ax.legend()
+plt.show()
+
+ax = plt.axes(projection='3d')
+ax.plot(Yeval, Xeval[:,0], Xeval[:,2], label="True value", marker='.', linestyle='None')
+ax.plot(Yhat, Xeval[:,0], Xeval[:,2], label="Prediction", marker='.', linestyle='None')
+ax.legend()
+plt.show()
---
+++ ML03/ex07/my_logistic_regression.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+def check_matrix(m, sizeX, sizeY, dim = 2):
+	if (not isinstance(m, np.ndarray)):
+		return False
+	if (m.ndim != dim or m.size == 0):
+		return False
+	if (sizeX != -1 and m.shape[0] != sizeX):
+		return False
+	if (dim == 2 and sizeY != -1 and m.shape[1] != sizeY):
+		return False
+	return True
+
+def sigmoid_(x):
+	"""
+	Compute the sigmoid of a vector.
+	Args:
+		x: has to be a numpy.ndarray of shape (m, 1).
+	Returns:
+		The sigmoid value as a numpy.ndarray of shape (m, 1).
+		None if x is an empty numpy.ndarray.
+	Raises:
+		This function should not raise any Exception.
+	"""
+	if (not check_matrix(x, -1, 1)):
+		return None
+	return 1 / (1 + np.exp(x * -1))	
+
+
+class MyLogisticRegression():
+	"""
+	Description:
+		My personnal logistic regression to classify things.
+	"""
+	def __init__(self, theta, alpha=0.001, max_iter=1000):
+		if ((not isinstance(alpha, float)) or (not isinstance(max_iter, int)) or max_iter < 0 or not check_matrix(theta, -1, 1)):
+			raise ValueError 
+		self.alpha = alpha
+		self.max_iter = max_iter
+		self.theta = theta
+
+	def predict_(self, x):
+		if (not check_matrix(x, -1, self.theta.shape[0] - 1)):
+			return None
+		copyx = np.insert(x, 0, 1, axis = 1)
+		return sigmoid_(np.dot(copyx, self.theta))
+
+	def loss_elem_(self, y, y_hat, eps=1e-15):
+		if (not check_matrix(y, -1, 1) or not check_matrix(y_hat, y.shape[0], 1) or not isinstance(eps, float)):
+			return None
+		copyYHat = y_hat.copy()
+		copyYHat = np.where(copyYHat == 1, copyYHat - eps, copyYHat)
+		copyYHat = np.where(copyYHat == 0, copyYHat + eps, copyYHat)
+		return y * np.log(copyYHat) + (1 - y) * np.log(1 - copyYHat)
+
+	def loss_(self, y, y_hat, eps=1e-15):
+		if (not check_matrix(y, -1, 1) or not check_matrix(y_hat, y.shape[0], 1) or not isinstance(eps, float)):
+			return None
+		copyYHat = y_hat.copy()
+		copyYHat = np.where(copyYHat == 1, copyYHat - eps, copyYHat)
+		copyYHat = np.where(copyYHat == 0, copyYHat + eps, copyYHat)
+		vec_one = np.ones(y.shape).reshape((-1, 1))
+		dot1 = np.dot(y.reshape(-1), np.log(copyYHat).reshape(-1))
+		dot2 = np.dot((vec_one - y).reshape(-1), np.log(vec_one - copyYHat).reshape(-1))
+		return (dot1 + dot2) / (-y.shape[0])
+
+	def log_gradient(self, x, y):
+		if (not check_matrix(x, -1, self.theta.shape[0] - 1) or not check_matrix(y, x.shape[0], 1)):
+			return None
+		copyX = np.insert(x, 0, 1, axis = 1)
+		return np.dot(copyX.T, sigmoid_(np.dot(copyX, self.theta)) - y) / x.shape[0]
+
+	def fit_(self, x, y):
+		if (not check_matrix(x, -1, self.theta.shape[0] - 1) or not check_matrix(y, x.shape[0], 1)):
+			return None
+		self.theta = self.theta.astype('float64')
+		copyX = x.copy().astype('float64')
+		copyY = y.copy().astype('float64')
+		for i in range(self.max_iter):
+			self.theta = self.theta - (self.alpha * self.log_gradient(copyX, copyY))
